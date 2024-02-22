eab22551c7878296a3a5a9dc4ae8c4f4d6982d8e
fuxiao <576101059@qq.com>
2024-02-22 10:08:38 +0800

    Adapt to the latest due framework

--- cluster/gate/main.go
+++ cluster/gate/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"github.com/dobyte/due/locate/redis/v2"
+	"github.com/dobyte/due/network/tcp/v2"
 	"github.com/dobyte/due/network/ws/v2"
 	"github.com/dobyte/due/registry/consul/v2"
 	"github.com/dobyte/due/transport/rpcx/v2"
 	"github.com/dobyte/due/v2"
 	"github.com/dobyte/due/v2/cluster/gate"
+	"github.com/dobyte/due/v2/flag"
+	"github.com/dobyte/due/v2/network"
 )
 
 func main() {
 	// 创建容器
 	container := due.NewContainer()
+
+	var server network.Server
 	// 创建服务器
-	server := ws.NewServer()
+	switch flag.String("protocol", "ws") {
+	case "tpc":
+		server = tcp.NewServer()
+	default:
+		server = ws.NewServer()
+	}
 	// 创建用户定位器
 	locator := redis.NewLocator()
 	// 创建服务发现
